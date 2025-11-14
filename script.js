57b38554abc747924bb91e9a91566fdbcc0e4af6
Norén <simon.noren@yahoo.com>
2025-11-14 14:24:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,64 @@
-const canvas = document.getElementById('fireCanvas');
+const canvas = document.getElementById('fog');
 const ctx = canvas.getContext('2d');
 
 function resizeCanvas() {
-  canvas.width = window.innerWidth;
-  canvas.height = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--fire-height'));
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 }
 resizeCanvas();
 window.addEventListener('resize', resizeCanvas);
 
-// Partiklar
-const colors = ['#9b3cff','#ff68d4','#ffb5f7','#c14bff'];
-const particles = [];
+// Partiklar för dimma
+const fogParticles = [];
+const colors = ['rgba(155, 60, 255, 0.05)', 'rgba(255, 104, 212, 0.05)', 'rgba(255, 181, 247,0.05)'];
 
-class Flame {
-  constructor() {
-    this.reset();
-  }
-  reset() {
-    this.x = Math.random() * canvas.width;
-    this.y = canvas.height + Math.random() * 50;
-    this.size = 8 + Math.random() * 12;
-    this.speedY = 1 + Math.random() * 2.5;
-    this.speedX = (Math.random() - 0.5) * 1.2;
-    this.alpha = 0.5 + Math.random() * 0.5;
-    this.color = colors[Math.floor(Math.random() * colors.length)];
-  }
-  update() {
-    this.y -= this.speedY;
-    this.x += this.speedX;
-    this.alpha -= 0.005;
-    if (this.alpha <= 0 || this.y < 0) this.reset();
-  }
-  draw() {
-    const gradient = ctx.createRadialGradient(this.x, this.y, 0, this.x, this.y, this.size);
-    gradient.addColorStop(0, this.color);
-    gradient.addColorStop(0.5, this.color + '80');
-    gradient.addColorStop(1, 'transparent');
-    ctx.fillStyle = gradient;
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-    ctx.fill();
-    ctx.closePath();
-  }
+class FogParticle {
+    constructor() {
+        this.reset();
+    }
+    reset() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.size = 50 + Math.random() * 150;
+        this.speedX = (Math.random() - 0.5) * 0.2;
+        this.speedY = (Math.random() - 0.5) * 0.1;
+        this.color = colors[Math.floor(Math.random() * colors.length)];
+    }
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+        if (this.x < -this.size || this.x > canvas.width + this.size ||
+            this.y < -this.size || this.y > canvas.height + this.size) {
+            this.reset();
+        }
+    }
+    draw() {
+        const gradient = ctx.createRadialGradient(this.x, this.y, 0, this.x, this.y, this.size);
+        gradient.addColorStop(0, this.color);
+        gradient.addColorStop(1, 'transparent');
+        ctx.fillStyle = gradient;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
+    }
 }
 
-// Skapa partiklar
-for (let i = 0; i < 200; i++) {
-  particles.push(new Flame());
+// Skapa 30 dimpartiklar
+for (let i = 0; i < 30; i++) {
+    fogParticles.push(new FogParticle());
 }
 
 // Animation
-function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  for (let p of particles) {
-    p.update();
-    p.draw();
-  }
-  requestAnimationFrame(animate);
+function animateFog() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    fogParticles.forEach(p => {
+        p.update();
+        p.draw();
+    });
+    requestAnimationFrame(animateFog);
 }
 
-animate();
+animateFog();
 
 
 
